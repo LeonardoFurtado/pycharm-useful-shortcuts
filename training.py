ba976073b57e04533e3ddb1ace0755bf0bcb0c97
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-10-23 15:43:53 +0000

    feat: add file to training shortcuts.

---
+++ training.py
@@ -0,0 +1,5 @@
+# Duplicate until four lines (CTRL + D) and delete one (ctrl + y)
+print("print one time")
+# select the last word (ctrol + w), rename and select complete string (ctrl + w 2 times)
+# move above line to the first ( ctrol shift up)
+# use ctrl left | right to go to first string and ctrl w to change
